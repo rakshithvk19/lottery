8dfd0a0b75dd62517122ab48e927fa3e16eddaea
Rakshith-Rajkumar <rakshithvk19@gmail.com>
2023-12-29 00:20:57 -0500

    login page styling added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound";
 import Landing from "./pages/Landing";
 import FAQ from "./pages/FAQ";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Route index element={<Landing />} />
         <Route path="*" element={<PageNotFound />} />
         <Route path="/FAQ" element={<FAQ />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Forgot_Password" element={<ForgotPassword />} />
       </Routes>
     </>
   );
